63b7c4623dbe65172a51b0f82ab0c7d6cca119e0
Antonio50gr <angaro1926@gmail.com>
2025-02-12 14:07:50 +0100

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+  <div>
+    <div className='Container bg-red-500 text-white'>
+      <h1>Hola</h1>
+    </div>
+  </div>
+  )
+}
+
+export default App
